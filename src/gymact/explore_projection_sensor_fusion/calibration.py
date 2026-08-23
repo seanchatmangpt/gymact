977f16deddb692901ffccec33f45ec5cd9c1ce2d
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-22 19:28:56 -0700

    style(explore): wrap calibration admission boundary

--- src/gymact/explore_projection_sensor_fusion/calibration.py
+++ src/gymact/explore_projection_sensor_fusion/calibration.py
@@ -14,7 +14,8 @@ class Calibration:
     ambiguity: Fraction
 
     def __post_init__(self) -> None:
-        if self.support < 0 or any(x < 0 or x > 1 for x in (self.false_current, self.false_stale, self.ambiguity)):
+        rates = (self.false_current, self.false_stale, self.ambiguity)
+        if self.support < 0 or any(rate < 0 or rate > 1 for rate in rates):
             raise FusionRefused("REFUSED_INVALID_CALIBRATION")
 
     @property
